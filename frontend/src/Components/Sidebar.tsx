19b6791abdb78a7e2187a741f59c3d03c8146d5a
HarshGupta <harshgupta0907@gmail.com>
2025-08-26 11:23:04 +0530

    Updated embedded link

---
+++ frontend/src/Components/Sidebar.tsx
@@ -0,0 +1,37 @@
+import { Shareicon } from "../icons/Shareicon";
+
+interface CardProps {
+    title: string;
+    link: string;
+    type: "twitter" | "youtube";
+}
+
+
+export function Card({ title, link, type }: CardProps) {
+    return <div className="p-4 bg-white rounded-md border-gray-200 max-w-72 border">
+        <div className="flex justify-between">
+            <div className="flex items-center text-md">
+                <div className="pr-2 text-gray-500">
+                    <a href={link} target="_blank"></a>
+                    <Shareicon />
+                </div>
+
+                {title}
+            </div>
+            <div className="flex items-center">
+                <div className="pr-2 text-gray-500">
+                    <Shareicon />
+                </div>
+                <div className="pr-2 text-gray-500">
+                    <Shareicon />
+                </div>
+            </div>
+        </div>
+        <div className="pt-4">
+            {type === "youtube" && <iframe className="w-full" src={link.replace("watch?v=6p8R6vFuFMI&ab_channel=100xDevs", "embed/6p8R6vFuFMI?si=p72gVv1jfHMoOOCS")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
+            {type === "twitter" && <blockquote className="twitter-tweet">
+                <a href={link.replace("x.com", "twitter.com")}></a>
+            </blockquote>}
+        </div>
+    </div>
+}
